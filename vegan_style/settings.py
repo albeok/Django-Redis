2853adaf965a2e2996d102d4c7b662234a528613
alberto <alberto_toscano97@yahoo.it>
2022-05-15 23:00:36 +0200

    debug False

--- vegan_style/settings.py
+++ vegan_style/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('secret_key'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
